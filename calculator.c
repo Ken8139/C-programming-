eef681e56c9cf7ce0a23e50e82c9219b4f78a857
Kenaz Mathukutty <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2025-01-29 15:26:47 +0530

    calculator

---
+++ calculator.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main() {
+  char op;
+  int a,b,res;
+  printf("\nenter the character +, -, *, /,%%  ");
+  scanf("%c",&op);
+  printf("\n enter two operands  ");
+  scanf("%d,%d",&a,&b);
+  switch (op){
+    case'+':
+        res=a+b;
+        printf("\n%d+%d=%d",a,b,res);
+    case'-':
+        res=a-b;
+        break;
+         printf("\n%d-%d=%d",a,b,res);
+    case'*':
+        res=a*b;
+        break;
+         printf("\n%d*%d=%d",a,b,res);
+    case'/':
+        res=a/b;
+        break;
+         printf("\n%d/%d=%d",a,b,res);
+    case'%':
+         printf("\n%d %% %d=%d",a,b,a%b);
+         break;
+    default:
+        printf("error incorrect operand value\n");
+    }
+        
+    return 0;
+}
+    
+  
