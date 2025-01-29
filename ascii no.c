7975f207e59f7db91c051d7406ae8e35133ed300
Kenaz Mathukutty <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2025-01-29 13:51:31 +0530

    integer

---
+++ ascii no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  printf("\nsize if integer=%ld",sizeof(int));
+  printf("\nsize if short integer=%ld",sizeof(long int));
+  printf("\nsize if long integer=%ld",sizeof(short int));
+  printf("\nsize if float=%ld",sizeof(float));
+  printf("\nsize if double=%ld",sizeof(double));
+  printf("\nsize if char=%ld\n",sizeof(char));
+  return 0;
+}
